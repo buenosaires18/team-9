41b3650095fb62295c009a77377052f4cf3eaa13
Gino <gimarceca@gmail.com>
2018-05-26 07:11:40 -0300

    Updated main script

--- App.js
+++ App.js
@@ -1,19 +1,78 @@
 import React from 'react';
-import { Constants } from 'expo';
-import { View, StatusBar, StyleSheet } from 'react-native';
+import { Constants, Video } from 'expo';
+import { View, StatusBar, StyleSheet, Button, Alert, Image, ScrollView } from 'react-native';
 import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view'; // 0.0.67
 
+import t from 'tcomb-form-native'; // 0.6.13
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  name: t.String,
+  surname: t.String,
+  username: t.String,
+  password: t.String,
+  dni: t.String,
+  birth: t.String,
+  city: t.String,
+  cp: t.String
+});
+
 const FirstRoute = () => (
-  <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
+  <View style={[styles.container, { backgroundColor: '#ff4081' }]} >
+  
+      <View style={[styles.imageContainer, { backgroundColor: '#ff4081' }]}>
+      <Image source={{uri: 'https://image.ibb.co/cD34Fo/BE_A_MENTOR_logo.png'}}
+      style={styles.imageCanvas} />
+      </View>
+      <View style={[styles.buttonContainer, { backgroundColor: '#ff4081' }]}>
+      <Button onPress={() => {
+       Alert.alert('Video here!');
+      }}
+      title="Press Me"
+      color="#841584"
+      accessibilityLabel="Learn more about this purple button"
+      />
+      </View>
+      </View>
 );
 const SecondRoute = () => (
-  <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
+  <View style={[styles.container, { backgroundColor: '#673ab7' }]} >
+    <ScrollView>
+    <Form type={User} />
+    </ScrollView>
+  </View>
+);
+
+const ThirdRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#00aeef' }]}>
+ 
+      <Video
+        source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+        
+        // pause not working
+        paused={this.state.paused}
+      onLoad={() => {
+      this.setState({
+        paused: true
+      });
+        }}
+        
+        rate={1.0}
+        volume={1.0}
+        muted={false}
+        resizeMode="cover"
+        shouldPlay
+        isLooping
+        style={{ width: 300, height: 300 }}
+      />
+  </View>
 );
 
 export default class TabViewExample extends React.Component {
   state = {
     index: 0,
-    routes: [{ key: '1', title: 'First' }, { key: '2', title: 'Second' }],
+    routes: [{ key: '1', title: 'Stage 1' }, { key: '2', title: 'Stage 2' }, { key: '3', title: 'Stage 3' }],
   };
 
   _handleIndexChange = index => {
@@ -25,6 +84,7 @@ export default class TabViewExample extends React.Component {
   _renderScene = SceneMap({
     '1': FirstRoute,
     '2': SecondRoute,
+    '3': ThirdRoute,
   });
 
   render() {
@@ -52,4 +112,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  imageContainer: {
+    height: '90%'
+  },
+  buttonContainer: {
+    height: '10%'
+  },
+  imageCanvas: {
+    flex: 1,
+  }
 });
